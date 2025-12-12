3cde3e63194f86aa9fc149223ce9d0e48cb03421
Robert <robert.rutta@gmail.com>
2025-12-12 13:10:48 -0500

    Corrected location of scss pre- and post- files

--- lib.php
+++ lib.php
@@ -71,9 +71,9 @@ function theme_crownboost_get_main_scss_content($theme) {
     $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
     
     // Pre CSS - this is loaded AFTER any prescss from the setting but before the main scss.
-    $pre = file_get_contents($CFG->dirroot . '/theme/boost/scss/pre.scss');
+    $pre = file_get_contents($CFG->dirroot . '/theme/crownboost/scss/pre.scss');
     // Post CSS - this is loaded AFTER the main scss but before the extra scss from the setting.
-    $post = file_get_contents($CFG->dirroot . '/theme/boost/scss/post.scss');
+    $post = file_get_contents($CFG->dirroot . '/theme/crownboost/scss/post.scss');
 
     // Combine them together.
     return $pre . "\n" . $scss . "\n" . $post;
